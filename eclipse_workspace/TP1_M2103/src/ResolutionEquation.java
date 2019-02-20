6e970ce30395a44f859c097bb34ee5cb114b541c
Biinod81 <45740233+Biinod81@users.noreply.github.com>
2019-02-20 12:04:14 +0100

    Add files via upload

--- eclipse_workspace/TP1_M2103/src/ResolutionEquation.java
+++ eclipse_workspace/TP1_M2103/src/ResolutionEquation.java
@@ -1,30 +1,30 @@
 import java.util.Scanner;
 
 public class ResolutionEquation {
-
 	public static void main(String[] args) {
-		Scanner entrée = new Scanner(System.in);
-		System.out.println("Coefficient a :");
-		float a = entrée.nextInt();
-		System.out.println("Coefficient b :");
-		float b = entrée.nextInt();
-		System.out.println("Coefficient c :");
-		float c = entrée.nextInt();
-		System.out.println("Résolution de l'équation : "+a+"x^2 + "+b+"x + "+c+" = 0");
-		float delta=(b*b)-(4*a*c);
-		System.out.println("discriminant: "+delta);
-		if (delta < 0) {
-			System.out.println("Aucune solution réelle");
+		Scanner entre = new Scanner(System.in);
+		System.out.println("Entrez a");
+		int a = entre.nextInt();
+		System.out.println("Entrez b");
+		int b = entre.nextInt();
+		System.out.println("Entrez c");
+		int c = entre.nextInt();
+		double delta = b*b -4*a*c;
+		if (delta > 0 ) {
+			double x1 = (-b + Math.sqrt(delta))/ 2*a ;
+			double x2 = (-b - Math.sqrt(delta))/2*a ;
+			System.out.println("x1 ="+x1+" x2 ="+x2);
+		}else{
+			if (delta < 0) {
+				System.out.println("Pas de solution car delta < 0");
+			}else {
+				if (delta == 0) {
+					double x3 = (-b / 2*a);
+					System.out.println("il n'y a qu'une seule solution qui est :"+x3);
+				}
+			}	
 		}
-		else if (delta == 0){
-			float resultat= (-b)/(2*a);
-			System.out.println("1 solution : "+resultat);
-		}
-		else {
-			float res1= (-b+sqrt(delta))/(2*a);
-			float res2= (-b-sqrt(delta))/(2*a);
-		}
-	
+		entre.close();
 	}
-
 }
+
